e5ccdd2750145502b3bad39f8d99e91018588b81
yuuki4649-0ss <kuroneko.magisteru.1111@gmail.com>
2023-12-26 14:27:02 +0900

    課題　税込計算

---
+++ src/kadai_java/Review01.java
@@ -0,0 +1,18 @@
+package kadai_java;
+
+public class Review01 {
+
+public static void main(String[] args) {
+    int zei_nuki = 100 ;
+    int zei = 8 ;
+    int zei_gaku = tax(zei_nuki , zei);
+    int zei_komi = zei_nuki + zei_gaku ;
+    System.out.println(zei_nuki + "円の商品の税込価格は" + zei_komi + "円" + "(消費税は" + zei_gaku + "円)です。" );
+
+    }
+public static int tax(int zei_nuki , int zei) {
+    int zei_gaku = (zei_nuki * zei ) / 100 ;
+    return zei_gaku ;
+
+    }
+}
